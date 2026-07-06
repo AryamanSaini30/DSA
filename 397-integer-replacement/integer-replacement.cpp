bd44a3d294e7e227302da48802b4a1e64cf6cbcf
Aryaman Saini <149478110+AryamanSaini30@users.noreply.github.com>
2026-07-06 10:50:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (79.47%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void recurse(long long argo, int& count, int amx) {
+        if(argo == 1) {
+            count = std::min(count, amx); // Minimum steps taken
+            return;
+        }
+        if(argo % 2 != 0) { 
+                        recurse(argo + 1, count, amx + 1); 
+            recurse(argo - 1, count, amx + 1); 
+        }
+        else {
+               recurse(argo / 2, count, amx + 1);
+        }
+    }
+
+    int integerReplacement(int n) {
+        long long argo = n;
+        int count = INT_MAX;
+        int amx = 0;
+        recurse(argo, count, amx);
+        return count;
+    }
+};
